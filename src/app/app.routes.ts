e3520c5685638d3bec4b8c2ec834b2c009dda19a
user-yeman <63607902+user-yeman@users.noreply.github.com>
2025-02-19 23:16:01 +0900

    yeman-login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '', redirectTo: 'login', pathMatch: 'full'
+    },
+    {
+        path : 'login', 
+        component: LoginComponent
+    },
+    {
+        path : '',
+        component: LayoutComponent,
+        children: [
+            {
+                path : 'dashboard',
+                component: DashboardComponent           }
+        ]
+
+    }
+];
