a3c4a35bdd66595ef5d58577af99f8d0409f0161
user-yeman <63607902+user-yeman@users.noreply.github.com>
2025-03-09 23:47:05 +0900

    added snackbar to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { LayoutComponent } from '../layout/layout.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule], //  Add FormsModule
+  imports: [FormsModule, MatSnackBarModule], // Add MatSnackBarModule
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
@@ -15,24 +15,34 @@ export class LoginComponent {
   email = '';
   password = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private snackBar: MatSnackBar) {}
 
   onLogin() {
     this.authService.login(this.email, this.password).subscribe(
       (res: any) => {
         this.authService.storeUserData(res.token, res.role);
         if (res.first_login) {
-          window.alert(`Welcome to the first login!`);
+          this.snackBar.open(`Welcome to the first login!`, 'Close', {
+            duration: 6000,
+            panelClass: ['snackbar-success'],
+            verticalPosition: 'top'
+          });
         } else {
-          window.alert(`Login successful! Role: ${res.role}`);
+          this.snackBar.open(`Login successful! Role: ${res.role}`, 'Close', {
+            duration: 6000,
+            panelClass: ['snackbar-success'],
+            verticalPosition: 'top'
+          });
         }
         console.log('Login successful', res);
         this.redirectToDashboard(res.role);
       },
       (error: any) => {
-        window.alert(
-          'Login failed. Please check your credentials and try again.'
-        );
+        this.snackBar.open('Login failed. Please check your credentials and try again.', 'Close', {
+          duration: 6000,
+          panelClass: ['snackbar-error'],
+          verticalPosition: 'top'
+        });
         console.error('Login failed', error);
       }
     );
