8bbca3a024bbf4bbd98d64eade28d27c964c3345
user-yeman <63607902+user-yeman@users.noreply.github.com>
2025-05-02 03:01:00 +0900

    fixed #1 reback auth.sevice as master

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -52,9 +52,6 @@ export class AuthService {
   getFirstLogin(): boolean {
     return this.cookieService.get('first_login') === 'true';
   }
-  getUserName(): string {
-    return this.cookieService.get('userName') || '';
-  }
 
   isAuthenticated(): Observable<boolean> {
     const token = this.cookieService.get('authToken');
